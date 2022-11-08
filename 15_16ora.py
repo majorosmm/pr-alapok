94779f3de359b841f7ac9d0502cdc13d2081d9ac
majorosmm <112930769+majorosmm@users.noreply.github.com>
2022-11-08 09:42:14 +0100

    Add files via upload

--- 15_16ora.py
+++ 15_16ora.py
@@ -34,11 +34,45 @@ for x in "banana":
 a = "hello world.asd2212dddada"
 print(len(a))
 print(a[11])
-print(a[len(a)-1])'''
+print(a[len(a)-1])
 
 a = 'hello world'
 szamlalo = 1
 for x in a:
     if szamlalo %2 == 0:
         print(a[szamlalo -1], end="")
-    szamlalo = szamlalo + 1
+    szamlalo = szamlalo + 
+
+    txt = "The best things in life are frese!"
+print("free" in txt) 
+
+b = "Hello, World!"
+print(b[7:14]) # llo
+
+c = "B"+b[1:8]
+print(c)
+
+b= "Hello, world"
+print (b[2:])
+
+a = "Hello, World!"
+print(a.upper())
+nagybetu = a.upper()
+print(nagybetu)
+
+a = "   Hello, World!    "
+print(a.strip())
+b = a.strip()
+print (len(a.strip()))
+
+a = "Jello, Jorld!"
+print(a)
+print(a.replace("J", "H"))
+a = "Hello, world!"
+print (a.split(","))
+lista = a.split(",")
+print (lista[1])
+b = "44;341;223;333;54544"
+print(b.split(";"))
+lista1 = b.split(";")
+print(lista1[3])'''
