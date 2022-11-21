cc4782086759e8e539fe7ce6942229564358576d
majorosmm <112930769+majorosmm@users.noreply.github.com>
2022-11-21 18:34:09 +0100

    Add files via upload

---
+++ hazifeladat19_20.py.py
@@ -0,0 +1,53 @@
+#--------------------------------------------
+# 2.feladat
+'''
+szam = 1
+hile szam <= 100:
+    print(szam)
+    szam = szam + 2
+'''
+#--------------------------------------------
+# 3.feladat
+'''
+szam = 1
+while szam <=100:
+    print(szam)
+    szam = szam + 3
+'''
+#--------------------------------------------
+# 4.feladat
+'''
+szam = 2
+while szam <= 100:
+    print(szam)
+    szam = szam + 2
+'''
+#--------------------------------------------
+# 5.feladat
+'''
+szam = 1
+while szam <= 100:
+    print(szam)
+    if szam %2 == 0:
+        szam = szam +1
+    else:
+        szam = szam + 2'''
+
+'''szam = 1
+while szam <= 100:
+    print(szam)
+    szam = szam +2
+'''
+#---------------------------------------------
+# 6.feladat
+
+i = ('h.a.z.i.f.e.l.a.d.a.t')
+print(len(i))
+
+
+while i <=21:
+    print(i)
+
+    
+   
+
