29b2ac2a31878c527fec377062001c6bcaf3cc8f
majorosmm <112930769+majorosmm@users.noreply.github.com>
2022-11-28 18:01:05 +0100

    Add files via upload

---
+++ hazi22.py
@@ -0,0 +1,39 @@
+#2.feladat
+'''
+for szam in range (1,101):
+    print(szam)
+'''
+#------------------------------------------------
+#3.feladat
+'''
+for szam in range (1,101,3):
+    print(szam)
+'''
+#------------------------------------------------
+#4.feladat
+'''
+szam =2
+print(szam)
+for szam in range(1,100):
+
+    if szam %2 == 0:
+        print(szam +2)
+'''
+#------------------------------------------------
+#5.feladat
+'''
+szam =1
+print(szam)
+for szam in range(1,100):
+
+    if szam %2 == 0:
+        print(szam +1)
+'''
+#------------------------------------------------
+#6.feladat
+
+szoveg = 'Hazi feladat!fjdajkfhsdfh'
+hozza = len(szoveg)
+print(hozza)
+for index in range(hozza,13) :
+    print(szoveg) 
