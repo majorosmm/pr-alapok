8f6df63a22b81d52a0f9236807525aaa18ae72f4
majorosmm <112930769+majorosmm@users.noreply.github.com>
2023-02-15 07:50:15 +0100

    Add files via upload

---
+++ feladat_1.py
@@ -0,0 +1,12 @@
+# Majoros Máté Martin 10.c  9-es
+#2023.02.15
+
+szam = int(input("Adj meg egy számot!: "))
+
+if szam > 0:
+    print("a megadott szám pozitiv. ")
+elif szam < 0:
+    print("a szam negativ. ")
+elif szam == 0:
+    print(" a megadott szam 0 . ")
+
