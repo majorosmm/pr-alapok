c49657e3d98a47d3fc855ae2a5bcaea8b690d377
majorosmm <112930769+majorosmm@users.noreply.github.com>
2023-02-22 07:38:50 +0100

    Add files via upload

---
+++ 2_feladat.py
@@ -0,0 +1,10 @@
+#Majoros Máté 10.c
+#2023.02.22
+#2.feladat
+
+szo = None
+while szo != "":
+    szo = input("Adj meg szavakat! Ha kilepnel a szo helyett üss ENTERT!")
+    print(szo)
+
+print("A program vége!")
